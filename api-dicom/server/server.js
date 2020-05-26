15d92e50d37ac9f8fc68b5dd93bb7c85eb5cd0b0
EriksonJAguiar <erjulioaguiar@gmail.com>
2020-05-26 02:43:23 -0300

    blockchain API

--- api-dicom/server/server.js
+++ api-dicom/server/server.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+const ipfsClient = require('ipfs-http-client');
+const { globSource } = ipfsClient
 var app = express();
+const ipfs = ipfsClient('');
 const fabricNetwork = require('./fabricNetwork')
 const enrollAdmin = require('../enrollAdmin')
 const registerUser = require('../registerUser')
@@ -8,17 +11,29 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.json());
 urlencoder = bodyParser.urlencoded({ extended: true });
 
-try {
-  enrollAdmin.enrollAdmin('hprovider', 'HProviderMSP');
-  enrollAdmin.enrollAdmin('research', 'ResearchMSP'); 
-} catch (error) {
-  console.log(error);
-}
 
+
+app.get('/api/initNetwork', urlencoder, async function (req, res) {
+  try {
+      enrollAdmin.enrollAdmin('hprovider', 'HProviderMSP');
+      enrollAdmin.enrollAdmin('research', 'ResearchMSP');
+    res.json({
+      status: 'True'
+    });
+    console.log('OK - Transaction has been submitted');
+  } catch (error) {
+    console.error(`Failed to evaluate transaction: ${error}`);
+    res.status(500).json({
+      error: error,
+      status: 'False'
+    });
+  }
+
+});
 
 app.post('/api/registerUser', urlencoder, async function (req, res) {
   try {
-    let result = registerUser.registerUser(req.body.org, req.body.user,req.body.msp);
+    let result = registerUser.registerUser(req.body.org, req.body.user, req.body.msp);
     res.json({
       status: 'True'
     });
@@ -32,15 +47,19 @@ app.post('/api/registerUser', urlencoder, async function (req, res) {
   }
 });
 
-app.post('/api/createDicom', urlencoder, async function (req, res) {
+app.post('/api/addAsset', urlencoder, async function (req, res) {
 
   try {
     const contract = await fabricNetwork.connectNetwork('connection-hprovider.json', '../../wallet/wallet-hprovider', req.body.user);
     console.log(req.body);
-    let tx = await contract.submitTransaction('createDicom', req.body.dicomId, req.body.typeExam, req.body.owner);
+    var currentdate = new Date();
+    let response = await contract.submitTransaction('addAsset', req.body.dicomID, req.body.patientID, req.body.patientFirstname, req.body.patientLastname, 
+                                              req.body.patientTelephone, req.body.patientAddress, req.body.patientAge, req.body.patientBirth, req.body.patientOrganization, 
+                                              req.body.patientMothername, req.body.patientReligion, req.body.patientSex, req.body.patientGender, req.body.patientInsuranceplan, 
+                                              req.body.patientWeigth, req.body.patientHeigth, req.body.machineModel, currentdate.getDate().toString());
     res.json({
       status: 'OK - Transaction has been submitted',
-      txid: tx.toString()
+      result: response.toString()
     });
     console.log('OK - Transaction has been submitted');
   } catch (error) {
@@ -52,10 +71,10 @@ app.post('/api/createDicom', urlencoder, async function (req, res) {
 
 });
 
-app.get('/api/readDicom/:dicomId', async function (req, res) {
+app.get('/api/getAsset/:dicomId', async function (req, res) {
   try {
     const contract = await fabricNetwork.connectNetwork('connection-hprovider.json', '../../wallet/wallet-hprovider', req.params.user.toString());
-    const result = await contract.evaluateTransaction('readDicom', req.params.dicomId.toString());
+    const result = await contract.evaluateTransaction('getAsset', req.params.dicomId.toString());
     let response = JSON.parse(result.toString());
     res.json({ result: response });
     console.log('OK - Query Successful');
@@ -67,15 +86,14 @@ app.get('/api/readDicom/:dicomId', async function (req, res) {
   }
 });
 
-app.post('/api/shareDicom', urlencoder, async function (req, res) {
-
+app.post('/api/shareAssetWithDoctor', urlencoder, async function (req, res) {
   try {
-    const contract = await fabricNetwork.connectNetwork('connection-research.json', '../../wallet/wallet-research', req.body.user);
+    const contract = await fabricNetwork.connectNetwork('connection-hprovider.json', '../../wallet/wallet-hprovider', req.body.user);
     console.log(req.body);
-    let tx = await contract.submitTransaction('shareDicom', req.body.tokenDicom, req.body.to, req.body.toOrganization, Date.now().toString());
+    let response = await contract.submitTransaction('shareAssetWithDoctor', req.body.patientID, req.body.doctorID, req.body.hashIPFS, req.body.dicomID);
     res.json({
       status: 'OK - Transaction has been submitted',
-      txid: tx.toString()
+      result: response.toString()
     });
     console.log('OK - Transaction has been submitted');
   } catch (error) {
@@ -87,10 +105,10 @@ app.post('/api/shareDicom', urlencoder, async function (req, res) {
 
 });
 
-app.get('/api/readAccessLog/:tokenDicom', async function (req, res) {
+app.get('/api/getSharedAssetWithDoctor/:hashIPFS', async function (req, res) {
   try {
     const contract = await fabricNetwork.connectNetwork('connection-research.json', '../../wallet/wallet-research', req.params.user.toString());
-    const result = await contract.evaluateTransaction('readAccessLog', req.params.tokenDicom.toString());
+    const result = await contract.evaluateTransaction('getSharedAssetWithDoctor', req.params.hashIPFS.toString());
     let response = JSON.parse(result.toString());
     res.json({ result: response });
     console.log('OK - Query Successful');
@@ -102,7 +120,95 @@ app.get('/api/readAccessLog/:tokenDicom', async function (req, res) {
   }
 })
 
+app.post('/api/requestAssetForResearcher', urlencoder, async function (req, res) {
+  try {
+    const contract = await fabricNetwork.connectNetwork('connection-research.json', '../../wallet/wallet-research', req.body.user);
+    console.log(req.body);
+    let response = await contract.submitTransaction('requestAssetForResearcher', req.body.amount.toString(), req.body.researchID, req.body.patientID);
+    res.json({
+      status: 'OK - Transaction has been submitted',
+      result: response.toString()
+    });
+    console.log('OK - Transaction has been submitted');
+  } catch (error) {
+    console.error(`Failed to evaluate transaction: ${error}`);
+    res.status(500).json({
+      error: error
+    });
+  }
 
+});
+
+app.post('/api/shareAssetForResearcher', urlencoder, async function (req, res) {
+  try {
+    const contract = await fabricNetwork.connectNetwork('connection-hprovider.json', '../../wallet/wallet-hprovider', req.body.user);
+    console.log(req.body);
+    let response = await contract.submitTransaction('shareAssetForResearcher', req.body.requestID.toString());
+    res.json({
+      status: 'OK - Transaction has been submitted',
+      result: response.toString()
+    });
+    console.log('OK - Transaction has been submitted');
+  } catch (error) {
+    console.error(`Failed to evaluate transaction: ${error}`);
+    res.status(500).json({
+      error: error
+    });
+  }
+
+});
+
+app.get('/api/getSharedAssetForResearcher', urlencoder, async function (req, res) {
+  try {
+    const contract = await fabricNetwork.connectNetwork('connection-research.json', '../../wallet/wallet-research', req.body.user);
+    let result = await contract.submitTransaction('getSharedAssetForResearcher', req.body.accessID.toString());
+    const response = JSON.parse(result.toString());
+    res.json({ result: response.toString() });
+    console.log('OK - Transaction has been submitted');
+  } catch (error) {
+    console.error(`Failed to evaluate transaction: ${error}`);
+    res.status(500).json({
+      error: error
+    });
+  }
+
+});
+
+app.get('/api/auditLog', urlencoder, async function (req, res) {
+  try {
+    const contract = await fabricNetwork.connectNetwork('connection-hprovider.json', '../../wallet/wallet-hprovider', req.body.user);
+    const result = await contract.submitTransaction('auditLog', req.body.tokenID.toString());
+    let response = JSON.parse(result.toString());
+    res.json({ result: response.toString() });
+    console.log('OK - Transaction has been submitted');
+  } catch (error) {
+    console.error(`Failed to evaluate transaction: ${error}`);
+    res.status(500).json({
+      error: error
+    });
+  }
+
+});
+
+
+app.post('/api/addIPFS', urlencoder, async function (req, res) {
+  try {
+    const response = await ipfs.add(globSource(req.body.path.toString(), { recursive: true }))
+    res.json({
+      status: 'OK - Transaction has been submitted',
+      result: response.toString()
+    });
+    console.log('OK - Transaction has been submitted');
+  } catch (error) {
+    console.error(`Failed to evaluate transaction: ${error}`);
+    res.status(500).json({
+      error: error
+    });
+  }
+
+});
+
+// For getting file we should use a local client
 
 
 
