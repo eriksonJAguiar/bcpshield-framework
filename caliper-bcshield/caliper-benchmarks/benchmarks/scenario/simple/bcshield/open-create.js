a0343c1bb9e2a3c088f624f5ad5746b8591ed452
EriksonJAguiar <erjulioaguiar@gmail.com>
2020-07-01 14:44:16 -0300

    try fix open create

--- caliper-bcshield/caliper-benchmarks/benchmarks/scenario/simple/bcshield/open-create.js
+++ caliper-bcshield/caliper-benchmarks/benchmarks/scenario/simple/bcshield/open-create.js
@@ -14,50 +14,33 @@
 
 'use strict';
 
-const { WorkloadModuleBase } = require('@hyperledger/caliper-core');
-module.exports.info = 'create dicom imaging';
-
-
-/**
- * Workload module for the benchmark round.
- */
-class CreateCarWorkload extends WorkloadModuleBase {
-    /**
-     * Initializes the workload module instance.
-     */
-    constructor() {
-        super();
-        this.txIndex = 0;
-    }
-
-    /**
-     * Assemble TXs for the round.
-     * @return {Promise<TxStatus[]>}
-     */
-    async submitTransaction() {
-        this.txIndex++;
-        // let carNumber = 'Client' + this.workerIndex + '_CAR' + this.txIndex.toString();
-        // let carColor = colors[Math.floor(Math.random() * colors.length)];
-        // let carMake = makes[Math.floor(Math.random() * makes.length)];
-        // let carModel = models[Math.floor(Math.random() * models.length)];
-        // let carOwner = owners[Math.floor(Math.random() * owners.length)];
-        var dcm_id = 'Patient_' + this.workerIndex + '_Dicom' + this.txIndex.toString();
-        
-        let args = {
-            chaincodeFunction: 'addAsset',
-            chaincodeArguments: [dcm_id, "2808886", "Jeff", "Slavech", "4221513", "310 South Crouse Avenue Syracuse NY", "54", "01-01-1966", "OHIP", "AAAAA", "None", "Male", "ASASSAS", "1780694000", "67.0", "1.77", "C3L-01285"],
-        };
-
-        return this.sutAdapter.invokeSmartContract(this.sutContext, 'dicom-cliper', '1', args, 30);
-    }
-}
+const logger = require('@hyperledger/caliper-core').CaliperUtils.getLogger('my-module');
+const { v1: uuidv4 } = require('uuid')
+// save the objects during init
+let bc, contx;
 
 /**
- * Create a new instance of the workload module.
- * @return {WorkloadModuleInterface}
- */
-function createWorkloadModule() {
-    return new CreateCarWorkload();
-}
+* Initializes the workload module before the start of the round.
+* @param {BlockchainInterface} blockchain The SUT adapter instance.
+* @param {object} context The SUT-specific context for the round.
+* @param {object} args The user-provided arguments for the workload module.
+*/
+module.exports.init = async (blockchain, context, args) => {
+    bc = blockchain;
+    contx = context;
+    logger.debug('Initialized workload module');
+};
 
-module.exports.createWorkloadModule = createWorkloadModule;
+module.exports.run = async () => {
+    let txArgs = {
+        chaincodeFunction: 'addAsset',
+        chaincodeArguments: [uuidv4(), "2808886", "Jeff", "Slavech", "4221513", "310 South Crouse Avenue Syracuse NY", "54", "01-01-1966", "OHIP", "AAAAA", "None", "Male", "ASASSAS", "1780694000", "67.0", "1.77", "C3L-01285"],
+    };
+    
+    return bc.invokeSmartContract(contx, 'dicom-caliper', '1', txArgs, 30);
+};
+
+module.exports.end = async () => {
+    // Noop
+    logger.debug('Disposed of workload module');
+};
