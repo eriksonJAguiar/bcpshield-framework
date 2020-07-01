637e94d26070d4598dbeafaed4b7bd3851ed4fc3
EriksonJAguiar <erjulioaguiar@gmail.com>
2020-07-01 15:41:17 -0300

    fix config values

--- caliper-bcshield/caliper-benchmarks/benchmarks/scenario/simple/bcshield/open-create.js
+++ caliper-bcshield/caliper-benchmarks/benchmarks/scenario/simple/bcshield/open-create.js
@@ -1,98 +1,105 @@
-
-/*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-* http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
-
-// 'use strict';
-
-// const { WorkloadModuleBase } = require('@hyperledger/caliper-core');
-// module.exports.info = 'create dicom imaging';
-
-
-// /**
-//  * Workload module for the benchmark round.
-//  */
-// class CreateCarWorkload extends WorkloadModuleBase {
-//     /**
-//      * Initializes the workload module instance.
-//      */
-//     constructor() {
-//         super();
-//         this.txIndex = 0;
-//     }
-
-//     /**
-//      * Assemble TXs for the round.
-//      * @return {Promise<TxStatus[]>}
-//      */
-//     async submitTransaction() {
-//         this.txIndex++;
-//         // let carNumber = 'Client' + this.workerIndex + '_CAR' + this.txIndex.toString();
-//         // let carColor = colors[Math.floor(Math.random() * colors.length)];
-//         // let carMake = makes[Math.floor(Math.random() * makes.length)];
-//         // let carModel = models[Math.floor(Math.random() * models.length)];
-//         // let carOwner = owners[Math.floor(Math.random() * owners.length)];
-//         var dcm_id = 'Patient_' + this.workerIndex + '_Dicom' + this.txIndex.toString();
-        
-//         let args = {
-//             chaincodeFunction: 'addAsset',
-//             chaincodeArguments: [dcm_id, "2808886", "Jeff", "Slavech", "4221513", "310 South Crouse Avenue Syracuse NY", "54", "01-01-1966", "OHIP", "AAAAA", "None", "Male", "ASASSAS", "1780694000", "67.0", "1.77", "C3L-01285"],
-//         };
-
-//         return this.sutAdapter.invokeSmartContract(this.sutContext, 'dicom-cliper', '1', args, 30);
-//     }
-// }
-
-// /**
-//  * Create a new instance of the workload module.
-//  * @return {WorkloadModuleInterface}
-//  */
-// function createWorkloadModule() {
-//     return new CreateCarWorkload();
-// }
-
-// module.exports.createWorkloadModule = createWorkloadModule;
-
-
 'use strict';
 
-const logger = require('@hyperledger/caliper-core').CaliperUtils.getLogger('my-module');
-const { v1: uuidv4 } = require('uuid')
-// save the objects during init
-let bc, contx;
+const { WorkloadModuleBase } = require('@hyperledger/caliper-core');
+
+const dic = 'abcdefghijklmnopqrstuvwxyz';
+let dicom_array = [];
 
 /**
-* Initializes the workload module before the start of the round.
-* @param {BlockchainInterface} blockchain The SUT adapter instance.
-* @param {object} context The SUT-specific context for the round.
-* @param {object} args The user-provided arguments for the workload module.
-*/
-module.exports.init = async (blockchain, context, args) => {
-    bc = blockchain;
-    contx = context;
-    logger.debug('Initialized workload module');
-};
+ * Workload module for the benchmark round.
+ */
+class Workload extends WorkloadModuleBase {
+    /**
+     * Initializes the workload module instance.
+     */
+    constructor() {
+        super();
+        this.txnPerBatch = 1;
+        this.initDicom = 0;
+        this.prefix = '';
+    }
 
-module.exports.run = async () => {
-    let txArgs = {
-        chaincodeFunction: 'addAsset',
-        chaincodeArguments: [uuidv4(), "2808886", "Jeff", "Slavech", "4221513", "310 South Crouse Avenue Syracuse NY", "54", "01-01-1966", "OHIP", "AAAAA", "None", "Male", "ASASSAS", "1780694000", "67.0", "1.77", "C3L-01285"],
-    };
-    
-    return bc.invokeSmartContract(contx, 'dicom-caliper', '1', txArgs, 30);
-};
+    /**
+     * Initialize the workload module with the given parameters.
+     * @param {number} workerIndex The 0-based index of the worker instantiating the workload module.
+     * @param {number} totalWorkers The total number of workers participating in the round.
+     * @param {number} roundIndex The 0-based index of the currently executing round.
+     * @param {Object} roundArguments The user-provided arguments for the round from the benchmark configuration file.
+     * @param {BlockchainInterface} sutAdapter The adapter of the underlying SUT.
+     * @param {Object} sutContext The custom context object provided by the SUT adapter.
+     * @async
+     */
+    async initializeWorkloadModule(workerIndex, totalWorkers, roundIndex, roundArguments, sutAdapter, sutContext) {
+        await super.initializeWorkloadModule(workerIndex, totalWorkers, roundIndex, roundArguments, sutAdapter, sutContext);
 
-module.exports.end = async () => {
-    // Noop
-    logger.debug('Disposed of workload module');
-};
+        if(!this.roundArguments.hasOwnProperty('dicom')) {
+            throw new Error('simple.open - \'ducom\' is missed in the arguments');
+        }
+
+        this.initDicom = this.roundArguments.dicom;
+        this.txnPerBatch = this.roundArguments.txnPerBatch || 1;
+        this.prefix = this.workerIndex.toString();
+    }
+
+    /**
+     * Generate string by picking characters from dic variable
+     * @param {*} number character to select
+     * @returns {String} string generated based on @param number
+     */
+    _get26Num(number){
+        let result = '';
+        while(number > 0) {
+            result += dic.charAt(number % 26);
+            number = Math.floor(number/26);
+        }
+        return result;
+    }
+
+    /**
+     * Generate unique dicom key for the transaction
+     * @returns {String} dicom key
+     */
+    _generateDicom() {
+        return this.prefix + this._get26Num(dicom_array.length+1);
+    }
+
+    /**
+     * Generates simple workload
+     * @returns {Object} array of json objects
+     */
+    _generateWorkload() {
+        let workload = [];
+        for(let i= 0; i < this.txnPerBatch; i++) {
+            let acc_id = this._generateDicom();
+            dicom_array.push(acc_id);
+
+            
+            workload.push({
+                chaincodeFunction: 'addAsset',
+                chaincodeArguments: [acc_id, "2808886", "Jeff", "Slavech", "4221513", "310 South Crouse Avenue Syracuse NY", "54", "01-01-1966", "OHIP", "AAAAA", "None", "Male", "ASASSAS", "1780694000", "67.0", "1.77", "C3L-01285"],
+            });
+        }
+        
+        return workload;
+    }
+
+    /**
+     * Assemble TXs for the round.
+     * @return {Promise<TxStatus[]>}
+     */
+    async submitTransaction() {
+        let args = this._generateWorkload();
+        return this.sutAdapter.invokeSmartContract(this.sutContext, 'dicom-caliper', '1', args, 100);
+    }
+}
+
+/**
+ * Create a new instance of the workload module.
+ * @return {WorkloadModuleInterface}
+ */
+function createWorkloadModule() {
+    return new Workload();
+}
+
+module.exports.createWorkloadModule = createWorkloadModule;
+module.exports.dicom_array = dicom_array;
