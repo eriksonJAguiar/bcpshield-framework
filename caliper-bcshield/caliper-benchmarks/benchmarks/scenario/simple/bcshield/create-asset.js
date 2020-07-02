6aacb7ec4a843932fe8adb691448c7efba6b2a15
EriksonJAguiar <erjulioaguiar@gmail.com>
2020-07-01 23:26:03 -0300

    new create asset version

--- caliper-bcshield/caliper-benchmarks/benchmarks/scenario/simple/bcshield/create-asset.js
+++ caliper-bcshield/caliper-benchmarks/benchmarks/scenario/simple/bcshield/create-asset.js
@@ -12,47 +12,116 @@
 * limitations under the License.
 */
 
+// 'use strict';
+
+// module.exports.info  = 'create Assets';
+
+// let dicom_array = [];
+// let txnPerBatch;
+// let initAsset;
+// let bc, contx;
+// module.exports.init = async function(blockchain, context, args) {
+//     // if(!args.hasOwnProperty('dicom')) {
+//     //     return Promise.reject(new Error('create asset - \'dicom\' is missed in the arguments'));
+//     // }
+
+//     if(!args.hasOwnProperty('txnPerBatch')) {
+//         args.txnPerBatch = 1;
+//     }
+//     //initMoney = args.money;
+//     txnPerBatch = args.txnPerBatch;
+//     bc = blockchain;
+//     contx = context;
+
+//     return Promise.resolve();
+// };
+
+
+// /**
+//  * Generate string by picking characters from dic variable
+//  * @param {*} number character to select
+//  * @returns {String} string generated based on @param number
+//  */
+
+// /**
+//  * Generate unique dicom key for the transaction
+//  * @returns {String} dicom key
+//  */
+// const dic = 'abcdefghijklmnopqrstuvwxyz';
+// /**
+//  * Generate string by picking characters from dic variable
+//  * @param {*} number character to select
+//  * @returns {String} string generated based on @param number
+//  */
+// function get26Num(number){
+//     let result = '';
+//     while(number > 0) {
+//         result += dic.charAt(number % 26);
+//         number = parseInt(number/26);
+//     }
+//     return result;
+// }
+
+// let prefix;
+// /**
+//  * Generate unique account key for the transaction
+//  * @returns {String} account key
+//  */
+// function generateAccount() {
+//     // should be [a-z]{1,9}
+//     if(typeof prefix === 'undefined') {
+//         prefix = get26Num(process.pid);
+//     }
+//     return prefix + get26Num(dicom_array.length+1);
+// }
+
+// /**
+//  * Generates simple workload
+//  * @returns {Object} array of json objects
+//  */
+// function generateWorkload() {
+//     let workload = [];
+//     for(let i= 0; i < txnPerBatch; i++) {
+//         let acc_id = generateAccount();
+//         dicom_array.push(acc_id);
+
+//         workload.push({
+//             chaincodeFunction: 'addAsset',
+//             chaincodeArguments: [acc_id,  "2808886", "Jeff", "Slavech", "4221513", "310 South Crouse Avenue Syracuse NY", "54", "01-01-1966", "OHIP", "AAAAA", "None", "Male", "ASASSAS", "1780694000", "67.0", "1.77", "C3L-01285"],
+//         });
+//     }
+//     return workload;
+// }
+
+// module.exports.run = function() {
+//     let args = generateWorkload();
+//     return bc.invokeSmartContract(contx, 'dicom-caliper', '1', args, 100);
+// };
+
+// module.exports.end = function() {
+//     return Promise.resolve();
+// };
+
+// module.exports.dicom_array = dicom_array;
+
 'use strict';
 
-module.exports.info  = 'create Assets';
+const contractId = 'dicom-caliper';
+const version = '1';
 
+let bc, ctx, clientArgs, clientIdx;
 let dicom_array = [];
-let txnPerBatch;
-let initAsset;
-let bc, contx;
-module.exports.init = async function(blockchain, context, args) {
-    // if(!args.hasOwnProperty('dicom')) {
-    //     return Promise.reject(new Error('create asset - \'dicom\' is missed in the arguments'));
-    // }
 
-    if(!args.hasOwnProperty('txnPerBatch')) {
-        args.txnPerBatch = 1;
-    }
-    //initMoney = args.money;
-    txnPerBatch = args.txnPerBatch;
+module.exports.init = async function(blockchain, context, args) {
     bc = blockchain;
-    contx = context;
+    ctx = context;
+    clientArgs = args;
+    clientIdx = context.clientIdx.toString();
 
     return Promise.resolve();
 };
 
-
-/**
- * Generate string by picking characters from dic variable
- * @param {*} number character to select
- * @returns {String} string generated based on @param number
- */
-
-/**
- * Generate unique dicom key for the transaction
- * @returns {String} dicom key
- */
 const dic = 'abcdefghijklmnopqrstuvwxyz';
-/**
- * Generate string by picking characters from dic variable
- * @param {*} number character to select
- * @returns {String} string generated based on @param number
- */
 function get26Num(number){
     let result = '';
     while(number > 0) {
@@ -75,30 +144,18 @@ function generateAccount() {
     return prefix + get26Num(dicom_array.length+1);
 }
 
-/**
- * Generates simple workload
- * @returns {Object} array of json objects
- */
-function generateWorkload() {
-    let workload = [];
-    for(let i= 0; i < txnPerBatch; i++) {
-        let acc_id = generateAccount();
-        dicom_array.push(acc_id);
-
-        workload.push({
-            chaincodeFunction: 'addAsset',
-            chaincodeArguments: [acc_id,  "2808886", "Jeff", "Slavech", "4221513", "310 South Crouse Avenue Syracuse NY", "54", "01-01-1966", "OHIP", "AAAAA", "None", "Male", "ASASSAS", "1780694000", "67.0", "1.77", "C3L-01285"],
-        });
-    }
-    return workload;
-}
-
 module.exports.run = function() {
-    let args = generateWorkload();
-    return bc.invokeSmartContract(contx, 'dicom-caliper', '1', args, 100);
+    let acc_id = generateAccount();
+    dicom_array.push(acc_id);
+    let myArgs = {
+        chaincodeFunction: 'addAsset',
+        chaincodeArguments: [acc_id,  "2808886", "Jeff", "Slavech", "4221513", "310 South Crouse Avenue Syracuse NY", "54", "01-01-1966", "OHIP", "AAAAA", "None", "Male", "ASASSAS", "1780694000", "67.0", "1.77", "C3L-01285"]
+    };
+
+    return bc.invokeSmartContract(ctx, contractId, version, myArgs, 60);
 };
 
-module.exports.end = function() {
+module.exports.end = async function() {
     return Promise.resolve();
 };
 
